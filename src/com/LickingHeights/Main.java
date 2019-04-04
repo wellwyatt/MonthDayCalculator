b36c5d4a24627ad886c603c544efed0861690e84
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-04-04 07:45:40 -0400

    initialized my ints and started applying them.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,16 +1,20 @@
 package com.LickingHeights;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+int userDay,userMonth,userYear;
+Scanner keyboard = new Scanner(System.in);
+System.out.println("What is the date of your Birth?");
+userDay = keyboard.nextInt();
 
-
-
-
+System.out.println("What is the month of your Birthday (as a number)");
+userMonth = keyboard.nextInt();
 
 
 
 
 
     }
+
 }
