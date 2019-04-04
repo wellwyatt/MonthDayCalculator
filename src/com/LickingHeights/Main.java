ff96bc61adee0dd987fb5d4d5ab7646d00c98456
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-04-04 08:54:10 -0400

    Added my first Method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,20 +1,38 @@
 package com.LickingHeights;
 import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
-int userDay,userMonth,userYear;
-Scanner keyboard = new Scanner(System.in);
-System.out.println("What is the date of your Birth?");
-userDay = keyboard.nextInt();
+        int userDay, userMonth, userYear, K;
 
-System.out.println("What is the month of your Birthday (as a number)");
-userMonth = keyboard.nextInt();
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What is the date of your Birth?");
+        userDay = keyboard.nextInt();
 
+        System.out.println("What is the month of your Birthday (as a number)");
+        userMonth = keyboard.nextInt();
 
+        System.out.println("What year were you born?");
+        userYear = keyboard.nextInt();
+
+        System.out.println(monthCalculator(userMonth));
+    }
+
+    public static int monthCalculator(int userMonth) {
+        if (userMonth == 1) {
+            return userMonth + 13;
+        } else if (userMonth == 2) {
+            return userMonth + 12;
+        }
+        return userMonth ;
 
 
 
     }
+public static int yearCalculator(int userYear){
+        K = userYear%100;
+
+}
+
 
 }
