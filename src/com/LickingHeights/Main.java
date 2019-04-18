735208ac6f5a4daaa2aba88971c9b0f3efc5e90c
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-04-18 08:42:34 -0400

    completed my number of days method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,18 +1,18 @@
 
 package com.LickingHeights;
 
-        import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int userDay, userMonth, userYear, userYearMod100, userYearDiv100, q, m, Zeller, h, k;
+        int userDay, userMonth, userYear, userYearMod100, userYearDiv100, q, Zeller, h;
 
         Scanner keyboard = new Scanner(System.in);
 
         while (true) {
             System.out.println("What is the month of your Birthday (as a number)");
             userMonth = keyboard.nextInt();
-            m = userMonth;
+
 
 
             System.out.println("What is the date of your Birth?");
@@ -21,28 +21,62 @@ public class Main {
 
             System.out.println("What year were you born?");
             userYear = keyboard.nextInt();
-            k = userYear;
 
-            k = yearCalculator(k, m);
 
-            m = monthCalculator(m);
+            userYear = yearCalculator(userYear, userMonth);
+
+            userMonth = monthCalculator(userMonth);
 
             q = userDay;
 
-            userYearMod100 = k % 100;
+            userYearMod100 = userYear % 100;
 
-            userYearDiv100 = k / 100;
+            userYearDiv100 = userYear / 100;
 
-            Zeller = 13 * (m + 1) / 5;
+            Zeller = 13 * (userMonth + 1) / 5;
             h = (q + Zeller + userYearMod100 + (userYearMod100 / 4) + (userYearDiv100 / 4) + 5 * userYearDiv100) % 7;
 
-            // System.out.println(h);
+            //System.out.println(m);
             System.out.println(nurseryRhyme(h));
+            System.out.println(numOfDays(userMonth));
         }
     }
 
+    public static String numOfDays(int userMonth) {
+        switch (userMonth) {
+            case 13:
+                return "In January there are 31 days";
+            case 14:
+                return "In February there are 28 days (29 days during leap year) ";
+            case 3:
+                return "In March there are 31 days.";
+            case 4:
+                return "In April there are 30 days";
+            case 5:
+                return "In May there are 31 days.";
+            case 6:
+                return "In June there are 30 days.";
+            case 7:
+                return "In July there are 31 days.";
+            case 8:
+                return "In August there are 31 days.";
+            case 9:
+                return "In September there are 30 days.";
+            case 10:
+                return "In October there are 31 days.";
+            case 11:
+                return "In November there are 30 days";
+            case 12:
+                return "in December there are 31 days.";
+            default:
+                return "Please enter a month." + (userMonth);
+        }
+
+    }
+
+
     public static int monthCalculator(int userMonth) {
-        if (userMonth <=2) {
+        if (userMonth <= 2) {
             return userMonth + 12;
         }
         return userMonth;
@@ -79,8 +113,7 @@ public class Main {
         }
 
 
-
-
     }
 
 }
+
